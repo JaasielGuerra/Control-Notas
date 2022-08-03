9496fbd19b73de640fd6263e806685b8b487254b
JaasielGuerra <jaasielguerra17@gmail.com>
2022-08-02 23:31:57 -0600

    configuracion estructura proyecto #67

---
+++ controlnotas/src/main/java/com/umg/controlnotas/controller/alumno/ConductaController.java
@@ -0,0 +1,9 @@
+package com.umg.controlnotas.controller.alumno;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/conducta")
+public class ConductaController {
+}
---
+++ controlnotas/src/main/java/com/umg/controlnotas/controller/evaluaciones/ConsultarEvaluacionesController.java
@@ -0,0 +1,9 @@
+package com.umg.controlnotas.controller.evaluaciones;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/consultar-evaluaciones")
+public class ConsultarEvaluacionesController {
+}
