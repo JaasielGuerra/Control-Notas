091a35cbc4233a9f4591d1e6ed7dcac598f87d5d
JaasielGuerra <jaasielguerra17@gmail.com>
2022-08-02 23:52:03 -0600

    navegacion consulta alumnos #67

--- controlnotas/src/main/java/com/umg/controlnotas/controller/alumno/ConsultarAlumnosController.java
+++ controlnotas/src/main/java/com/umg/controlnotas/controller/alumno/ConsultarAlumnosController.java
@@ -1,9 +1,20 @@
 package com.umg.controlnotas.controller.alumno;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value = "/consultar-alumnos")
 public class ConsultarAlumnosController {
+
+    @GetMapping
+    public String consultarAlumnos(Model model) {
+
+        model.addAttribute("saludo", "Hola a todos....");
+
+        return "/alumno/consultar-alumnos";
+    }
+
 }
