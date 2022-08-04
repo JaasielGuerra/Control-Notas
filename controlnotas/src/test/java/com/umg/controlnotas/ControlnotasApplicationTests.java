709b15bd580c64f852125aa4997bd858b5d0da02
Angel Chacon <angel>
2022-08-04 11:44:56 -0600

    pllantilla para registrar y consultar alumno #3

--- controlnotas/src/test/java/com/umg/controlnotas/ControlnotasApplicationTests.java
+++ controlnotas/src/test/java/com/umg/controlnotas/ControlnotasApplicationTests.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ControlnotasApplicationTests {
+class
+ControlnotasApplicationTests {
 
 	@Test
 	void contextLoads() {
