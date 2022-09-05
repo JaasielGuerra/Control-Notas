bd4513fc6772b5cdacca017be9d071f9259db41d
JaasielGuerra <jaasielguerra17@gmail.com>
2022-09-04 22:01:15 -0600

    consultar plan trabajo #15

--- controlnotas/src/main/java/com/umg/controlnotas/repository/PlanTrabajoRepository.java
+++ controlnotas/src/main/java/com/umg/controlnotas/repository/PlanTrabajoRepository.java
@@ -1,8 +1,11 @@
 package com.umg.controlnotas.repository;
 
 import com.umg.controlnotas.model.PlanTrabajo;
+import com.umg.controlnotas.model.custom.PlanTrabajoConsultar;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PlanTrabajoRepository extends JpaRepository<PlanTrabajo, Long> {
 
@@ -10,5 +13,6 @@ public interface PlanTrabajoRepository extends JpaRepository<PlanTrabajo, Long> 
     @Query(value = "SELECT func_existe_plan_trabajo(?1,?2)", nativeQuery = true)
     int existsByIdBimestreIdAndIdGradoIdAndEstado(long bimestre, long id_grado);
 
+    List<PlanTrabajoConsultar> findByEstado(int estado);
 
 }
