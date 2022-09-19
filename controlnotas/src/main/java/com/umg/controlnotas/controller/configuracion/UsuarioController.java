9081bb6ae1c63bf38913e7eafbbf08b826a320a9
Angel Chacon <angel>
2022-09-19 14:45:27 -0600

    Formulario para crear, editar y consultar usuarios #111

--- controlnotas/src/main/java/com/umg/controlnotas/controller/configuracion/UsuarioController.java
+++ controlnotas/src/main/java/com/umg/controlnotas/controller/configuracion/UsuarioController.java
@@ -5,8 +5,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/coonsultar-usuarios")
+@RequestMapping(value = "/usuarios")
 public class UsuarioController {
 
+    @GetMapping(value = "/nuevo")
+    public String NuevoUsuario(){
+        return "configuraciones/registrar-usuario";
+    }
 
+
+    @GetMapping(value = "/consultar")
+    public String ConsultarUsuarios(){
+        return "configuraciones/consultar-usuarios";
+    }
+
+    @GetMapping(value = "/editar")
+    public String EditarUsuario(){
+        return "configuraciones/editar-usuario";
+    }
 }
