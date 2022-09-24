60ae1a769d7ce53ae4282aef9d9382807d40602b
Angel Chacon <angel>
2022-09-23 21:36:16 -0600

    realizar eliminar libros #16

--- controlnotas/src/main/java/com/umg/controlnotas/services/LibroService.java
+++ controlnotas/src/main/java/com/umg/controlnotas/services/LibroService.java
@@ -2,6 +2,7 @@ package com.umg.controlnotas.services;
 
 import com.umg.controlnotas.model.dto.LibroDto;
 import com.umg.controlnotas.model.dto.ResponseDataDto;
+import liquibase.pro.packaged.L;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -12,4 +13,10 @@ public interface LibroService {
     ResponseDataDto registrarNuevoLibro(LibroDto libroDto);
 
     List<LibroDto> consultarLibros();
+
+
+
+
+    @Transactional
+    ResponseDataDto eliminarLibro(Long id);
 }
