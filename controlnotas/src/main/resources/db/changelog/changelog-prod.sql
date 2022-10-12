fc915947a2a4909da4154634157474177b2b7d01
jaasiel17 <jaasielguerra17@gmail.com>
2022-10-11 23:16:39 -0600

    Unificar tablas control_actitudinal e historial_actitudinal y Relacionar bimestre con control_actitudinal #49

--- controlnotas/src/main/resources/db/changelog/changelog-prod.sql
+++ controlnotas/src/main/resources/db/changelog/changelog-prod.sql
@@ -55,3 +55,43 @@ END
 
 -- changeset liquibase:jaasiel-3
 ALTER TABLE db_control_notas.detalle_listado MODIFY COLUMN id_detalle_listado BIGINT auto_increment NOT NULL;
+
+
+-- changeset liquibase:jaasiel-4
+DROP TABLE `db_control_notas`.`historial_actitudinal`;
+
+-- changeset liquibase:jaasiel-5
+DROP TABLE `db_control_notas`.`control_actitudinal`;
+
+-- changeset liquibase:jaasiel-6
+CREATE TABLE IF NOT EXISTS `db_control_notas`.`control_actitudinal` (
+                                                                        `id_control_actitudinal` BIGINT NOT NULL AUTO_INCREMENT,
+                                                                        `id_materia` BIGINT NOT NULL,
+                                                                        `id_alumno` BIGINT NOT NULL,
+                                                                        `descripcion` VARCHAR(150) NOT NULL,
+                                                                        `puntos_restados` DOUBLE(16 , 2 ) NOT NULL COMMENT 'cantidad de puntos que se agregan',
+                                                                        `puntos_sumados` DOUBLE(16 , 2 ) NOT NULL COMMENT 'cantidad de puntos que se quitan',
+                                                                        `puntos_actuales` DOUBLE(16 , 2 ) NOT NULL,
+                                                                        `fecha` DATE NOT NULL,
+                                                                        `fecha_commit` DATE NOT NULL,
+                                                                        `hora_commit` TIME NOT NULL,
+                                                                        `id_usuario` BIGINT NOT NULL,
+                                                                        `id_bimestre` BIGINT NOT NULL,
+                                                                        PRIMARY KEY (`id_control_actitudinal`),
+                                                                        INDEX `fk_control_actitudinal_materia1_idx` (`id_materia` ASC),
+                                                                        INDEX `fk_control_actitudinal_alumno1_idx` (`id_alumno` ASC),
+                                                                        INDEX `fk_control_actitudinal_usuario1_idx` (`id_usuario` ASC),
+                                                                        INDEX `fk_control_actitudinal_bimestre1_idx` (`id_bimestre` ASC),
+                                                                        CONSTRAINT `fk_control_actitudinal_materia1` FOREIGN KEY (`id_materia`)
+                                                                            REFERENCES `db_control_notas`.`materia` (`id_materia`)
+                                                                            ON DELETE NO ACTION ON UPDATE NO ACTION,
+                                                                        CONSTRAINT `fk_control_actitudinal_alumno1` FOREIGN KEY (`id_alumno`)
+                                                                            REFERENCES `db_control_notas`.`alumno` (`id_alumno`)
+                                                                            ON DELETE NO ACTION ON UPDATE NO ACTION,
+                                                                        CONSTRAINT `fk_control_actitudinal_usuario1` FOREIGN KEY (`id_usuario`)
+                                                                            REFERENCES `db_control_notas`.`usuario` (`id_usuario`)
+                                                                            ON DELETE NO ACTION ON UPDATE NO ACTION,
+                                                                        CONSTRAINT `fk_control_actitudinal_bimestre1` FOREIGN KEY (`id_bimestre`)
+                                                                            REFERENCES `db_control_notas`.`bimestre` (`id_bimestre`)
+                                                                            ON DELETE NO ACTION ON UPDATE NO ACTION
+)  ENGINE=INNODB;
